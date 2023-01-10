793e003d62a41611e65028783f853694b600c13e
Andres vega <jandresvega@itsa.edu.co>
2023-01-10 18:06:13 -0500

    Estructurando Carpetas iniciales

---
+++ src/components/ItemTodo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ItemTodo = () => {
+    return (
+        <div>
+            <button>-</button>
+            <span>Tarea 1</span>
+            <button>x</button>
+        </div>
+    );
+};
+
+export {ItemTodo};
---
+++ src/containers/TodosItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodosItems = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export {TodosItems};
